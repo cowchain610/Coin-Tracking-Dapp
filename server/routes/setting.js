a31e5de7122ce249d335bfc15063a4983917ea1f
cowchain610 <cowchain610@gmail.com>
2024-10-04 10:06:03 +0000

    fix

--- server/routes/setting.js
+++ server/routes/setting.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const settingController = require('../controllers/settingController');
+const express = require('express');
 
 const router = new express.Router();
 
